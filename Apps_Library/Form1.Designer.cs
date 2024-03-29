abe4af0da4a714e04489ab36c02626f81ed5faae
tekater <tekateplay1@yandex.ru>
2024-03-29 23:24:47 +0900

    Library DONE (semi)

--- Apps_Library/Form1.Designer.cs
+++ Apps_Library/Form1.Designer.cs
@@ -28,26 +28,27 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.panelInfo = new System.Windows.Forms.Panel();
+			this.components = new System.ComponentModel.Container();
 			this.btnAdd = new System.Windows.Forms.Button();
 			this.btnClose = new System.Windows.Forms.Button();
 			this.btnDel = new System.Windows.Forms.Button();
+			this.panelInfo = new System.Windows.Forms.Panel();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnApp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnAppPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.lblInfo = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
 			this.panel1 = new System.Windows.Forms.Panel();
+			this.btnLaunch = new System.Windows.Forms.Button();
+			this.panelInfo.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// panelInfo
-			// 
-			this.panelInfo.BackColor = System.Drawing.SystemColors.AppWorkspace;
-			this.panelInfo.Location = new System.Drawing.Point(178, 12);
-			this.panelInfo.Name = "panelInfo";
-			this.panelInfo.Size = new System.Drawing.Size(610, 394);
-			this.panelInfo.TabIndex = 1;
 			// 
 			// btnAdd
 			// 
 			this.btnAdd.Location = new System.Drawing.Point(12, 415);
 			this.btnAdd.Name = "btnAdd";
-			this.btnAdd.Size = new System.Drawing.Size(137, 23);
+			this.btnAdd.Size = new System.Drawing.Size(158, 23);
 			this.btnAdd.TabIndex = 2;
 			this.btnAdd.Text = "Add App";
 			this.btnAdd.UseVisualStyleBackColor = true;
@@ -61,6 +62,7 @@
 			this.btnClose.TabIndex = 3;
 			this.btnClose.Text = "Close";
 			this.btnClose.UseVisualStyleBackColor = true;
+			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
 			// 
 			// btnDel
 			// 
@@ -70,38 +72,113 @@
 			this.btnDel.TabIndex = 0;
 			this.btnDel.Text = "Delete";
 			this.btnDel.UseVisualStyleBackColor = true;
+			this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+			// 
+			// panelInfo
+			// 
+			this.panelInfo.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.panelInfo.Controls.Add(this.richTextBox1);
+			this.panelInfo.Controls.Add(this.lblInfo);
+			this.panelInfo.Location = new System.Drawing.Point(215, 12);
+			this.panelInfo.Name = "panelInfo";
+			this.panelInfo.Size = new System.Drawing.Size(572, 390);
+			this.panelInfo.TabIndex = 4;
+			this.panelInfo.Visible = false;
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			// 
+			// listView1
+			// 
+			this.listView1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnApp,
+            this.columnAppPath});
+			this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(13, 12);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(157, 390);
+			this.listView1.TabIndex = 5;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.List;
+			this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+			this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+			// 
+			// lblInfo
+			// 
+			this.lblInfo.AutoSize = true;
+			this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.lblInfo.Location = new System.Drawing.Point(3, 15);
+			this.lblInfo.Name = "lblInfo";
+			this.lblInfo.Size = new System.Drawing.Size(178, 37);
+			this.lblInfo.TabIndex = 0;
+			this.lblInfo.Text = "Information";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.richTextBox1.Location = new System.Drawing.Point(10, 55);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(556, 332);
+			this.richTextBox1.TabIndex = 1;
+			this.richTextBox1.Text = "";
 			// 
 			// panel1
 			// 
-			this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-			this.panel1.Location = new System.Drawing.Point(13, 13);
+			this.panel1.BackColor = System.Drawing.Color.Black;
+			this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.panel1.Location = new System.Drawing.Point(188, 0);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(136, 390);
-			this.panel1.TabIndex = 4;
+			this.panel1.Size = new System.Drawing.Size(13, 455);
+			this.panel1.TabIndex = 6;
+			// 
+			// btnLaunch
+			// 
+			this.btnLaunch.Location = new System.Drawing.Point(215, 415);
+			this.btnLaunch.Name = "btnLaunch";
+			this.btnLaunch.Size = new System.Drawing.Size(99, 23);
+			this.btnLaunch.TabIndex = 7;
+			this.btnLaunch.Text = "Launch";
+			this.btnLaunch.UseVisualStyleBackColor = true;
+			this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.ClientSize = new System.Drawing.Size(800, 448);
+			this.Controls.Add(this.btnLaunch);
 			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.panelInfo);
 			this.Controls.Add(this.btnDel);
 			this.Controls.Add(this.btnClose);
 			this.Controls.Add(this.btnAdd);
-			this.Controls.Add(this.panelInfo);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.panelInfo.ResumeLayout(false);
+			this.panelInfo.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-		private System.Windows.Forms.Panel panelInfo;
 		private System.Windows.Forms.Button btnAdd;
 		private System.Windows.Forms.Button btnClose;
 		private System.Windows.Forms.Button btnDel;
+		private System.Windows.Forms.Panel panelInfo;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader columnApp;
+		private System.Windows.Forms.ColumnHeader columnAppPath;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label lblInfo;
 		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button btnLaunch;
 	}
 }
 
--- Apps_Library/Form1.cs
+++ Apps_Library/Form1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Security.Policy;
+using System.Diagnostics;
+
 
 namespace Apps_Library
 {
 	public partial class Form1 : Form
 	{
+		ImageList img = new ImageList();
 		public Form1()
 		{
 			InitializeComponent();
@@ -19,6 +24,69 @@ namespace Apps_Library
 
 		private void btnAdd_Click(object sender, EventArgs e)
 		{
+			OpenFileDialog open = new OpenFileDialog();
+
+			open.DefaultExt = ".exe";
+			open.Multiselect = false;
+			open.Filter = "Executable | *.exe";
+
+			open.InitialDirectory = Environment.GetEnvironmentVariable("%SYSTEMDRIVE%");
+
+			open.ShowDialog();
+			//MessageBox.Show(open.FileName, "Initial path", MessageBoxButtons.OK);
+			
+			ListViewItem lvi = new ListViewItem(new string[] { open.SafeFileName});
+			lvi.SubItems.Add(open.FileName);
+
+			Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(open.FileName);
+			listView1.SmallImageList = img;
+
+			img.Images.Add(icon);
+			lvi.ImageIndex = img.Images.Count - 1;
+			listView1.Items.Add(lvi);
+
+			//MessageBox.Show(i, "Initial path", MessageBoxButtons.OK);
+		}
+		private void btnDel_Click(object sender, EventArgs e)
+		{
+			listView1.FocusedItem.Remove();
+			panelInfo.Visible = false;
+		}
+
+		private void btnClose_Click(object sender, EventArgs e)
+		{
+			Form1.ActiveForm.Close();
+		}
+		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			panelInfo.Visible = true;
+			DirectoryInfo dir = new DirectoryInfo(Path.GetDirectoryName(listView1.FocusedItem.SubItems[1].Text));
+			FileInfo[] file = dir.GetFiles();
+
+			long fileLength = file[0].Length;
+
+			DateTime dt = file[0].CreationTime;
+			DateTime lat = file[0].LastAccessTime;
+			
+			string filename = file[0].Name;
+			string fullname = file[0].FullName;
+
+			richTextBox1.Text = "FileName: " + filename + "\n\nPath: " + fullname + "\n\nCreationTime: " + dt.ToString() + "\n\nLastAccesTime: " + lat.ToString() + "\n\nWeight: " + fileLength;
+		}
+
+		private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Process.Start(listView1.FocusedItem.SubItems[1].Text);
+
+			//MessageBox.Show(listView1.FocusedItem.SubItems[1].Text, "Initial path", MessageBoxButtons.OK);
+		}
+
+		private void btnLaunch_Click(object sender, EventArgs e)
+		{
+			if(listView1.Focused)
+			{
+			Process.Start(listView1.FocusedItem.SubItems[1].Text);
+			}
 		}
 	}
 }
